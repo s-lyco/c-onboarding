9e171163354ed4d0d9b8ed37039552fb41dbb40d
KalpeshSipify <kalpesh@sipify.in>
2023-10-21 18:53:41 +0530

    updating code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-"use client";
+"use client"; // Ensure this code runs on the client-side
 import { useEffect, useState } from "react";
 import Head from "next/head";
 
+// Declare global interface for Facebook SDK
 declare global {
   interface Window {
-    fbAsyncInit: () => void;
-    FB: any;
+    fbAsyncInit: () => void; // Initialize function for Facebook SDK
+    FB: any; // Facebook SDK object
   }
 }
 
@@ -13,7 +14,7 @@ export default function Home() {
   const [fbLoaded, setFbLoaded] = useState(false);
 
   useEffect(() => {
-    // Load Facebook SDK
+    // Load Facebook SDK if not already loaded
     if (!fbLoaded) {
       const script = document.createElement("script");
       script.src = "https://connect.facebook.net/en_US/sdk.js";
@@ -28,30 +29,31 @@ export default function Home() {
             appId: "650341763798014",
             autoLogAppEvents: true,
             xfbml: true,
-            version: "v17.0",
+            version: "v17.0", // Facebook SDK version
           });
-          setFbLoaded(true); 
+          setFbLoaded(true); // Mark Facebook SDK as loaded
         };
       };
 
+      // Append the script to the document head
       document.head.appendChild(script);
     }
   }, [fbLoaded]);
 
-  // Facebook Login with JavaScript SDK
+  // Function to launch WhatsApp signup using Facebook Login
   function launchWhatsAppSignup() {
     if (window.FB) {
       window.FB.login(
         function (response: { authResponse: { accessToken: any } }) {
           if (response.authResponse) {
             const accessToken = response.authResponse.accessToken;
-            console.log(accessToken);
+            console.log(accessToken); // Log the access token
           } else {
             console.log("User cancelled login or did not fully authorize.");
           }
         },
         {
-          scope: "whatsapp_business_management",
+          scope: "whatsapp_business_management", // Request WhatsApp Business Management permission
           extras: {
             feature: "whatsapp_embedded_signup",
             setup: {
